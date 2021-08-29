459a9570c89f1137fe193d0c5b9f36b86d147362
larrykwon <koallarry11@kaist.ac.kr>
2021-08-29 22:05:56 +0900

    강의 시작

--- src/main/java/Hellojpa/Entity/Member.java
+++ src/main/java/Hellojpa/Entity/Member.java
@@ -5,35 +5,11 @@ import java.util.Date;
 
 @Entity
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private Long id;
 
     @Column(name = "USERNAME")
     private String name;
-    private int age;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date regDate;
-
-    @Enumerated(EnumType.STRING)
-    private MemberType memberType;
-
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", team=" + team +
-                ", regDate=" + regDate +
-                ", memberType=" + memberType +
-                '}';
-    }
 
     public Long getId() {
         return id;
@@ -43,43 +19,11 @@ public class Member {
         this.id = id;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
-
-    public MemberType getMemberType() {
-        return memberType;
-    }
-
-    public void setMemberType(MemberType memberType) {
-        this.memberType = memberType;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
     }
 }
--- src/main/java/Hellojpa/Main.java
+++ src/main/java/Hellojpa/Main.java
@@ -1,15 +1,11 @@
 package Hellojpa;
 
 import Hellojpa.Entity.Member;
-import Hellojpa.Entity.MemberType;
-import Hellojpa.Entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,34 +19,13 @@ public class Main {
         //트랜잭션을 시작,데이터베이스에서 접근해서 커넥션을 들고온 다음 트랜잭션을 실행행
         tx.begin();
     try {
-        Team team = new Team();
-        team.setName("teamA");
-        em.persist(team);
-
         Member member = new Member();
-//        member.setId(1L);
-        member.setName("안녕하세요");
-
-        member.setMemberType(MemberType.User);
-        member.setRegDate(new Date());
+        member.setId(101L);
+        member.setName("HelloJPA");
         em.persist(member);
 
-        team.getMembers().add(member);
-//
-//        member.setTeam(team);
-        em.flush();
-        em.clear();
 
-//        Member findMember = em.find(Member.class, member.getId());
-//        Team findTeam = findMember.getTeam();
-//
-//        findTeam.getName();
-//
-//        List<Member> members = findTeam.getMembers();
-//        for(Member member1 : members){
-//            System.out.println("member1 = "+ member1);
-//        }
-//        em.persist(member);
+
         tx.commit();
     }catch (Exception e) {
         //transaction이 끝나면 entitymanager를 닫음으로서 데이터베이스 리소스를 반환한다.
