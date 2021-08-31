6be0cc909ff2c3cf57219820268d9c1b86c9bfb6
larrykwon <koallarry11@kaist.ac.kr>
2021-08-31 23:53:14 +0900

    Lazy 옵션 및 Cascade를 이용해서 lifeCycle맞추기

---
+++ src/main/java/Hellojpa/Entity/BaseEntity.java
@@ -0,0 +1,45 @@
+package Hellojpa.Entity;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private String createdBy;
+    private LocalDateTime createDate;
+    private String lastModifedBy;
+    private LocalDateTime lastModifiedDate;
+
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifedBy() {
+        return lastModifedBy;
+    }
+
+    public void setLastModifedBy(String lastModifedBy) {
+        this.lastModifedBy = lastModifedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
